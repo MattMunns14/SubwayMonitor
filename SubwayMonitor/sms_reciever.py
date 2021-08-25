f7f3517c81168eaf61c827262754dfc901ec7b70
Matt Munns <matthewmunns0@gmail.com>
2021-08-25 18:05:55 -0400

    put request in table

--- SubwayMonitor/sms_reciever.py
+++ SubwayMonitor/sms_reciever.py
@@ -1,6 +1,8 @@
 import boto3
 import os
 import json
+import time
+from .utils import dict_to_dynamo_json
 
 SUPPORTED_TRAINS_AND_DIRECTIONS = [('C', 'N'), ('C', 'S')]
 
@@ -8,11 +10,13 @@ SUPPORTED_TRAINS_AND_DIRECTIONS = [('C', 'N'), ('C', 'S')]
 def handle_sns_event(event):
 
     sns_client = boto3.client('sns')
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    message_as_tuple = tuple(message['messageBody'].split())
+    number = message['originationNumber']
 
-    message = json.loads(event['Records'][0]['Sns']['Message'])['messageBody']
-    message_as_tuple = tuple(message.split())
     if message_as_tuple in SUPPORTED_TRAINS_AND_DIRECTIONS:
-        write_monitoring_request_to_dynamo(message_as_tuple)
+
+        write_monitoring_request_to_dynamo(message_as_tuple, number)
         response = sns_client.publish(
             TargetArn=os.environ['SMS_SENDER_TOPIC_ARN'],
             Message=f'Monitoring the {message_as_tuple[0]} heading {message_as_tuple[1]}'
@@ -26,5 +30,17 @@ def handle_sns_event(event):
         )
 
 
-def write_monitoring_request_to_dynamo(message_as_tuple):
-    pass
+def write_monitoring_request_to_dynamo(message_as_tuple, number):
+
+    dynamo_client = boto3.client('dynamodb')
+    request_dict = {
+        'timestamp': time.time(),
+        'status': 'open',
+        'train': message_as_tuple[0],
+        'direction': message_as_tuple[1],
+        'requester': number
+    }
+    dynamo_client.put_item(
+        Item=dict_to_dynamo_json(request_dict),
+        Table=os.environ['DYNAMODB_TABLE']
+    )
