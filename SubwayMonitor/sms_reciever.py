5fb6a37d0ff19807e3e75e092ec945dd257e87d6
Matt Munns <matthewmunns0@gmail.com>
2021-08-25 17:16:48 -0400

    dynamo table

--- SubwayMonitor/sms_reciever.py
+++ SubwayMonitor/sms_reciever.py
@@ -2,7 +2,7 @@ import boto3
 import os
 import json
 
-SUPPORTED_TRAINS_AND_DIRECTIONS = [('C', 'N')]
+SUPPORTED_TRAINS_AND_DIRECTIONS = [('C', 'N'), ('C', 'S')]
 
 
 def handle_sns_event(event):
@@ -12,7 +12,7 @@ def handle_sns_event(event):
     message = json.loads(event['Records'][0]['Sns']['Message'])['messageBody']
     message_as_tuple = tuple(message.split())
     if message_as_tuple in SUPPORTED_TRAINS_AND_DIRECTIONS:
-
+        write_monitoring_request_to_dynamo(message_as_tuple)
         response = sns_client.publish(
             TargetArn=os.environ['SMS_SENDER_TOPIC_ARN'],
             Message=f'Monitoring the {message_as_tuple[0]} heading {message_as_tuple[1]}'
@@ -24,3 +24,7 @@ def handle_sns_event(event):
             TargetArn=os.environ['SMS_SENDER_TOPIC_ARN'],
             Message=f'Message not understood or train not supported'
         )
+
+
+def write_monitoring_request_to_dynamo(message_as_tuple):
+    pass
